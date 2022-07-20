e616603fded88d36ba8f926cd63c61df96e7c0c0
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-07-20 23:26:02 +0900

    theme provider 색 추가

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,9 +1,10 @@
 export const theme = {
-  main: "",
-  grey300: "",
-  grey500: "",
-  grey700: "",
-  grey900: "",
-  black: "",
-  white: "",
+  main: "#009688",
+  grey300: "#F6F6F6",
+  grey500: "#DEDEDE",
+  grey700: "#8D8D8D",
+  grey900: "#2F2F2F",
+  black: "#000000",
+  white: "#FFFFFF",
+  system_red: "#F2624E",
 };
