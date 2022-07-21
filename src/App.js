50f2d45be8b911dcb8267cda30c70ec7789a9c23
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-07-21 10:10:07 +0900

    splash screen 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+import { ThemeProvider } from "styled-components/native";
 import { theme } from "./utils/theme";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import { Image } from "react-native";
+
+const cacheImages = (images) => {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
 
 const App = () => {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  const [isReady, setIsReady] = useState(false);
+
+  const _loadAsset = async () => {
+    const imageAsset = cacheImages([require("../assets/splash.png")]);
+    await Promise.all([...imageAsset]);
+  };
+  return isReady ? (
+    <ThemeProvider theme={theme}></ThemeProvider>
+  ) : (
+    <AppLoading
+      startAsync={_loadAsset}
+      onFinish={() => setIsReady(true)}
+      onError={console.warn}
+    />
+  );
 };
 
 export default App;
